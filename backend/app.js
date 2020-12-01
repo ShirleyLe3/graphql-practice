a64e5f2dad1694fe3c99658d76c4047fc610cd2b
Drew Ross <mail.drew.ross@gmail.com>
2020-12-01 14:03:44 -0800

    Add and configure test /graphql endpoint.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('API Running.');
-})
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Cooking', 'Sailing', 'Coding']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  }
+}));
 
 app.listen(8000);
